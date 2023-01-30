6654c83d5eb5efb4206b16396715068837826b2b
rufff1 <rufatzg@code.edu.az>
2023-01-29 17:41:54 -0800

    Home page modified

--- Gosto/Controllers/HomeController.cs
+++ Gosto/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Gosto.DAL;
+using Gosto.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,30 @@ namespace Gosto.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult>  Index()
+        {
+            HomeVM homeVM = new HomeVM
+            {
+                Sliders = await _context.Sliders.Where(s => s.IsDeleted == false).ToListAsync(),
+                ProductCategories = await _context.ProductCategories.Where(c => c.IsDeleted == false).ToListAsync(),
+                Teams = await _context.Teams.Where(t => t.IsDeleted == false).ToListAsync(),
+                Blogs = await _context.Blogs
+                .Include(b=> b.BlogCategory)
+                .Where(b => b.IsDeleted == false).ToListAsync(),
+                SellingProduct = await _context.Products.Where(p => p.IsDeleted == false && p.IsSellingProduct).Take(8).ToListAsync(),
+                TrendingProduct = await _context.Products.Where(p => p.IsDeleted == false && p.IsTrendingProduct).ToListAsync(),
+
+
+            };
+
+
+            return View(homeVM);
         }
     }
 }
