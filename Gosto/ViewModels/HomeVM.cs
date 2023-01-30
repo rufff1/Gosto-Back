eb5fa5e53d5e3db04ae58bd440cc46f29713555a
rufff1 <rufatzg@code.edu.az>
2023-01-29 16:45:57 -0800

    Home Page fixed

---
+++ Gosto/ViewModels/HomeVM.cs
@@ -0,0 +1,19 @@
+﻿using Gosto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gosto.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Slider> Sliders { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+        public IEnumerable<Product> SellingProduct { get; set; }
+        public IEnumerable<Product> TrendingProduct { get; set; }
+        public IEnumerable<Team> Teams { get; set; }
+        public IEnumerable<Blog> Blogs { get; set; }
+
+    }
+}
